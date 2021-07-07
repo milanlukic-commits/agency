152efd55a952e847b7b7609ebf7b9e549797764c
milanlukic_commitse <milanlukic.se@gmail.com>
2021-07-07 21:47:32 +0200

    izmene

---
+++ main.js
@@ -0,0 +1,11 @@
+var waypoint = new Waypoint({
+    element: document.getElementById('skills'),
+    handler: function() {
+      var progress = document.querySelectorAll('.progress-bar');
+      progress[0].setAttribute('style', 'width:99%;transition-duration: 1s;');
+      progress[1].setAttribute('style', 'width:95%;transition-duration: 2s;');
+      progress[2].setAttribute('style', 'width:79%;transition-duration: 3s;');
+      progress[3].setAttribute('style', 'width:69%;transition-duration: 4s;');
+    },
+    offset: '90%'
+});
